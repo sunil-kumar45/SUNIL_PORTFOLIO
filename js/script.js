e0fc5b43587d9f3127969dad006cdbe18ea652e0
RAMAVATH SUNILKUMAR <ramavathsunilkumar4545@gmail.com>
2025-07-19 23:08:07 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*=============================Typing animation=====================*/
+var typed = new Typed(".typing",{
+    strings:["Student","Coder","Web Designer", "Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
